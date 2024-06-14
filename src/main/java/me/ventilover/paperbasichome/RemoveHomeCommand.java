fae5717fc3d7290c5afd19254fc0cb2a7e67ef8f
BuildTools <unconfigured@null.spigotmc.org>
2024-06-14 23:12:50 +0200

    New class for the delhome Comand

---
+++ src/main/java/me/ventilover/paperbasichome/RemoveHomeCommand.java
@@ -0,0 +1,55 @@
+package me.ventilover.paperbasichome;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class RemoveHomeCommand extends Command {
+
+    public RemoveHomeCommand(){
+        super("delhome");
+        this.description = "Command to remove a home";
+    }
+
+    @Override
+    public boolean execute(@NotNull CommandSender commandSender, @NotNull String s, @NotNull String[] strings) {
+
+        //check for a home name
+        if (!checkIfValidCommand(commandSender,strings)){
+            return true; //don't run the command further
+        }
+        Player player = (Player) commandSender;
+
+        if (!player.hasPermission("paperbasichome.delhome")){
+            player.sendMessage(HomeManager.getInstance().makeErrorMessage("You do not have valid Permissions"));
+            return true;
+        }
+        String homeName = strings[0]; //the home name from the argument
+        try {
+            HomeManager.getInstance().removeHome(player,homeName);
+        } catch (Exception ex) {
+            player.sendMessage(HomeManager.getInstance().makeErrorMessage("No such home to delete!"));
+            return true;
+        }
+
+        return true;
+    }
+
+    public boolean checkIfValidCommand(CommandSender commandSender, String[] strings){
+        if (commandSender instanceof  Player player){
+            if (strings.length != 1){
+                player.sendMessage(HomeManager.getInstance().makeErrorMessage("Please enter a valid home name"));
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+        else {
+            commandSender.sendMessage("Only players can use this");
+            return false;
+        }
+    }
+}
