135551e8a8dfb1c1291c6bd813b1cfa381bd590a
BuildTools <unconfigured@null.spigotmc.org>
2024-06-14 17:18:57 +0200

    updated new methods for the hashmap system

--- src/main/java/me/ventilover/paperbasichome/HomeManager.java
+++ src/main/java/me/ventilover/paperbasichome/HomeManager.java
@@ -7,13 +7,16 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 
 public class HomeManager { //singelton class for management
 
     private static HomeManager instance;
 
-    private static final HashMap<Player, PlayerHomes> playerHomesHashMapHashMap = new HashMap<>();
+    public static HashMap<UUID,PlayerHomes> playerIdHashMap = new HashMap<>();
+
+    private static final HashMap<Player, PlayerHomes> playerHomesHashMap = new HashMap<>();
 
     private final HashMap<Player,Integer> playerTasks = new HashMap<>();
 
@@ -21,6 +24,13 @@ public class HomeManager { //singelton class for management
         //private constructor to interrupt init
     }
 
+    public void attachPlayerHomesHashmap(Player player, PlayerHomes playerHomes){
+        playerHomesHashMap.put(player,playerHomes); //class to add something into the session hashmap
+    }
+
+    public void detachPlayerHomesHashMap(Player player){
+        playerHomesHashMap.remove(player); //remove the player from the session (used on leave listener)
+    }
 
     public static HomeManager getInstance() { //to get the homemanager
         if(instance==null){
@@ -33,24 +43,39 @@ public class HomeManager { //singelton class for management
         return getPlayerHomesClass(player).checkIfPlayerHasHomes();
     }
 
+    public HashMap<UUID, PlayerHomes> getPlayerIdHashMap() {
+        return playerIdHashMap;
+    }
+
     public PlayerHomes getPlayerHomesClass(Player player){ //method to get the home location of a player
         // (useful for teleportation)
-        return playerHomesHashMapHashMap.get(player);
+        return playerHomesHashMap.get(player);
 
     }
 
-    public Home getPlayerHome(Player player,String homeName) throws Exception { //get the player home can cause exception when no such homename exist
+    public PlayerHomes getPlayerHomesClassFromId(UUID uuid){
+        return playerIdHashMap.get(uuid);
+    }
+
+    public Home getPlayerHome(Player player,String homeName) throws Exception { //get the player home
+        // can cause exception
+        // when no such home name exists
         PlayerHomes playerHomesClass= getPlayerHomesClass(player);
         return playerHomesClass.getHomeByName(homeName);
     }
 
     public void addPlayerHome(Player player,Location location,String homeName){ //method to add a new home
-        playerHomesHashMapHashMap.get(player).createPlayerHome(location,homeName);
+        playerHomesHashMap.get(player).createPlayerHome(location,homeName);
         player.sendMessage("Created a new home!");
     }
 
-    public void createPlayerHomesClass(Player player){ //method to create a playerhomesclass for a new player
-        playerHomesHashMapHashMap.put(player,new PlayerHomes());
+    public boolean hasPlayerHomes(UUID uuid){
+        return playerIdHashMap.containsKey(uuid);
+    }
+
+    public void createPlayerHomesClass(Player player){ //method to create a playerhomes class for a new player
+        //creates a new playerhomes instance
+        playerIdHashMap.put(player.getUniqueId(),new PlayerHomes());//safe it into the hashmap
     }
 
     public HashMap<Player, Integer> getPlayerTasks() {
